6b82477188e826d133a99151773f3ed74fe1b58d
yejwoo <yejinwoo.me@gmail.com>
2024-05-26 17:33:22 +0900

    feat: prisma 커넥션 풀 생성

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-const db = new PrismaClient();
+const db = new PrismaClient({
+    log: ['info', 'warn', 'error'],
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        }
+    }
+});
 export default db;
