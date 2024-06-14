421e7c8a1a1c30df3c571c84225778bf04548395
yejwoo <yejinwoo.me@gmail.com>
2024-06-14 23:15:10 +0900

    chore: 구글 이미지 경로 추가
    Related to #22

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
             hostname: 'dummyimage.com',
             pathname: '/**',
           },
+          {
+            protocol: 'https',
+            hostname: 'lh3.googleusercontent.com',
+            pathname: '/**',
+          },
       ],
     },
   };
