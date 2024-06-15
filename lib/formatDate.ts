63650e750d75807d9de85cf5274a960e1888354b
yejwoo <yejinwoo.me@gmail.com>
2024-06-16 00:26:54 +0900

    feat: 날짜 포맷 함수 파라미터 추가

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,9 +1,16 @@
-export default function formatDate(dateString: string): string {
+export default function formatDate(
+  dateString: string,
+  addHour: boolean = false
+): string {
   const date = new Date(dateString);
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  if (!addHour) {
+    return `${year}.${month}.${day}`;
+  }
   return `${year}.${month}.${day}. ${hours}:${minutes}`;
 }
