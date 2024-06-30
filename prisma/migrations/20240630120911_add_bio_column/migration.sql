27c61e47d7c4edb201ffaa2ddccc628a74af5857
yejwoo <yejinwoo.me@gmail.com>
2024-06-30 21:09:42 +0900

    feat: bio 컬럼 추가
    Related to #25

---
+++ prisma/migrations/20240630120911_add_bio_column/migration.sql
@@ -0,0 +1,23 @@
+-- AlterTable
+ALTER TABLE "Comment" ALTER COLUMN "createdAt" SET DEFAULT NOW(),
+ALTER COLUMN "updatedAt" SET DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE "Image" ALTER COLUMN "createdAt" SET DEFAULT NOW(),
+ALTER COLUMN "updatedAt" SET DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE "Like" ALTER COLUMN "createdAt" SET DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE "Post" ALTER COLUMN "createdAt" SET DEFAULT NOW(),
+ALTER COLUMN "updatedAt" SET DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE "ReplyComment" ALTER COLUMN "createdAt" SET DEFAULT NOW(),
+ALTER COLUMN "updatedAt" SET DEFAULT NOW();
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "bio" TEXT,
+ALTER COLUMN "createdAt" SET DEFAULT NOW(),
+ALTER COLUMN "updatedAt" SET DEFAULT NOW();
